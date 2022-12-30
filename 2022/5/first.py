10f07cf7986c434706215b3fcc61e70662c7562b
pragun Bajracharya <pragunbaj99@gmail.com>
2022-12-30 17:38:35 -0500

    Added solution of problem 5

---
+++ 2022/5/first.py
@@ -0,0 +1,41 @@
+with open('input.txt') as file:
+    lines = [line.rstrip() for line in file]
+
+# [P]     [L]         [T]
+# [L]     [M] [G]     [G]     [S]
+# [M]     [Q] [W]     [H] [R] [G]
+# [N]     [F] [M]     [D] [V] [R] [N]
+# [W]     [G] [Q] [P] [J] [F] [M] [C]
+# [V] [H] [B] [F] [H] [M] [B] [H] [B]
+# [B] [Q] [D] [T] [T] [B] [N] [L] [D]
+# [H] [M] [N] [Z] [M] [C] [M] [P] [P]
+#  1   2   3   4   5   6   7   8   9
+
+stack = [
+    ['H', 'B', 'V', 'W', 'N', 'M', 'L', 'P'],
+    ['M', 'Q', 'P'],
+    ['N', 'D', 'B', 'G', 'F', 'Q', 'M', 'L'],
+    ['Z', 'T', 'F', 'Q', 'M', 'W', 'G'],
+    ['M', 'T', 'H', 'P'],
+    ['C', 'B', 'M', 'J', 'D', 'H', 'G', 'T'],
+    ['M', 'N', 'B', 'F', 'V', 'R'],
+    ['P', 'L', 'H', 'M', 'R', 'G', 'S'],
+    ['P', 'D', 'B', 'C', 'N']
+]
+
+for x in lines:
+    fromStack = int(x.split(' ')[3]) - 1
+    toStack = int(x.split(' ')[5]) - 1
+    toMove = int(x.split(' ')[1])
+    # print(toStack, fromStack, toMove)
+    fromReversed = stack[fromStack][::-1]
+    newFrom = stack[fromStack][::-1][toMove:][::-1]
+    stack[toStack] = stack[toStack] + fromReversed[:toMove]
+    stack[fromStack] = newFrom
+
+allTop = ''
+
+for x in stack:
+    allTop = allTop + x[-1]
+
+print(allTop)
